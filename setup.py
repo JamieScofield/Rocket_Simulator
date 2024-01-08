a24990b4215790a567114b6f9b069cf0a6f4b7b1
jamie <jamies_1048@outlook.com>
2024-01-08 15:44:04 +0000

    adding tox

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup, find_packages
-import os
-env = os.getenv('PROFILES', "local")
+
+
 
 test_requires = [
     "mock",
     "pytest",
-    "pytest-cov",
-    "parameterized"
+    "pytest-cov"
 ]
+setup(
+    name="Rocket_Simulator",
+    version='0.5',
+    author='Jamie Scofield',
+    packages=find_packages(where="src/main/python"),
+    tests_require=test_requires,
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        test_requires
+    ]
+)
